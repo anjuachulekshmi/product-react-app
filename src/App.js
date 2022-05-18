39c1e1675a77533d07347eb6217cb9d6c70a3dd1
anjuachulekshmi <anjuachulekshmi836@gmail.com>
2022-05-18 06:13:17 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Productentry from './Component/Productentry';
 import Viewproduct from './Component/Viewproduct';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Productsearch from './Component/Productsearch';
 
 function App() {
   return (
    <div>
      
-     <Viewproduct  />
+     <BrowserRouter>
+   <Routes>
+     <Route path='/' exact element={<Productentry />} />
+
+     
+     <Route path='/search' exact element={<Productsearch/>} />
+     <Route path='/viewbook' exact element={<Viewproduct/>} />
+
+    
+     </Routes>
+     </BrowserRouter>
+   
    </div>
   );
 }
